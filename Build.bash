7b7a944c35b0b952580873d0ec8b5132607b9323
sosoeeee <809953371@qq.com>
2025-01-10 07:42:27 +0000

    add required depedencies in Build. bash

--- Build.bash
+++ Build.bash
@@ -1,6 +1,14 @@
-# sudo rm -rf build
+sudo rm -rf build
 mkdir build
 # sudo source /opt/ros/noetic/setup.bashCreating build group
+
+# dependencies
+sudo apt-get update
+sudo apt-get install cmake
+sudo apt-get install build-essential
+sudo apt-get install libusb-1.0
+sudo apt-get install liblog4cxx-dev
+
 cd build
 cmake -G "Unix Makefiles" ..
 make
